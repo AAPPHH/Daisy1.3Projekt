d24bab9e098740acad883fbe0a039a350be419fa
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-11-27 11:24:17 +0100

    John (#5)
    * basic stuff
    
    * try
    
    * Update and rename class_board to class_board.py
    
    * quatsch

--- class_bot_1.py
+++ class_bot_1.py
@@ -4,9 +4,9 @@ class GomokuBot:
         # Initialize any necessary variables or data structures
         pass
 
-    def make_move(self, board):
-        # Implement the logic for the bot to make a move on the board
+    def make_move(self, row, col):
         pass
+
 
     def evaluate_position(self, board):
         # Implement the evaluation function to assess the current position on the board
