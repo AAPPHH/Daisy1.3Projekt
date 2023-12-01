24fb705b3859dd0798d7931ce88bd0d5bbd41857
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-01 13:35:42 +0100

    Fix print_board method to display integer values

--- class_board.py
+++ class_board.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Board:
-    def __init__(self, m, n, k, board):
+    def __init__(self, m=5, n=5, k=4):
         self.m = m
         self.n = n
         self.k = k
@@ -9,7 +9,7 @@ class Board:
 
     def print_board(self):
         for row in self.board:
-            print(' '.join(row))
+            print(' '.join(str(int(val)) for val in row))
 
     def place_piece(self, row, col, piece):
         if self.is_valid_move(row, col):
@@ -18,26 +18,32 @@ class Board:
         return False
 
     def is_valid_move(self, row, col):
-        if 0 <= row < self.size and 0 <= col < self.size and self.board[row][col] == ' ':
+        m, n = self.board.shape
+        if 0 <= row < m and 0 <= col < n and self.board[row][col] == 0:
             return True
         return False
 
     def is_winner(self, piece):
-        # Check rows
-        for row in self.board:
-            if row.count(piece) == self.size:
+        # Zeilen überprüfen
+        if np.any(np.all(self.board == piece, axis=1)):
+            return True
+
+        # Spalten überprüfen
+        if np.any(np.all(self.board == piece, axis=0)):
+            return True
+
+        # Hauptdiagonale überprüfen, wenn das Brett quadratisch ist
+        if self.board.shape[0] == self.board.shape[1]:
+            if np.all(np.diag(self.board) == piece):
                 return True
 
-        # Check columns
-        for col in range(self.size):
-            if [self.board[row][col] for row in range(self.size)].count(piece) == self.size:
+            # Nebendiagonale überprüfen
+            if np.all(np.diag(np.fliplr(self.board)) == piece):
                 return True
-
-        # Check diagonals
-        if [self.board[i][i] for i in range(self.size)].count(piece) == self.size:
-            return True
-        if [self.board[i][self.size - 1 - i] for i in range(self.size)].count(piece) == self.size:
-            return True
 
         return False
 
+Spielbrett = Board()
+Spielbrett.print_board()
+Spielbrett.place_piece(0, 0, 1)
+Spielbrett.print_board()
