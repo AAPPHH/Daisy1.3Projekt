0daf219cf05cb086b647480db2cc8e1bbb740c10
Marc-Maxelon <151784489+Marc-Maxelon@users.noreply.github.com>
2023-12-03 00:45:35 +0100

    Update class_bot_1.py
    First try of coding the bot

--- class_bot_1.py
+++ class_bot_1.py
@@ -1,11 +1,187 @@
 
 class GomokuBot:
-    def __init__(self):
-        # Initialize any necessary variables or data structures
-        pass
+    def __init__(self, name, row, col):
+        self.name = name
+        self.row = row
+        self.col = col
 
     def make_move(self, board):
-        # Implement the logic for the bot to make a move on the board
+        import random  # Import the random module
+        random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+        return random_row  # Return the random number
+        random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+        return random_col  # Return the random number
+        point = (random_row, random_col)  # Create a tuple of the random numbers
+        return point  # Return the tuple
+        import class_board  # Import the class_board module
+        board = class_board.Board(5, 5)  # Create an instance of the Board class
+        return board  # Return the instance
+        if point == 0:
+            random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+            return random_row  # Return the random number
+            random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+            return random_col  # Return the random number
+            point = (random_row, random_col)  # Create a tuple of the random numbers
+            return point  # Return the tuple
+            import class_board  # Import the class_board module
+            board = class_board.Board(5, 5)  # Create an instance of the Board class
+            return board  # Return the instance
+            if point == 0:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+                if point == 0:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 1:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 2:
+                board_after_move = board.replace(0, point)
+                return board_after_move
+            elif point == 1:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+                if point == 0:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 1:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 2:
+                board_after_move = board.replace(0, point)
+                return board_after_move
+            elif point == 2:
+                board_after_move = board.replace(0, point)
+                return board_after_move
+        elif point == 1:
+            random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+            return random_row  # Return the random number
+            random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+            return random_col  # Return the random number
+            point = (random_row, random_col)  # Create a tuple of the random numbers
+            return point  # Return the tuple
+            import class_board  # Import the class_board module
+            board = class_board.Board(5, 5)  # Create an instance of the Board class
+            return board  # Return the instance
+            if point == 0:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+                if point == 0:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 1:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 2:
+                board_after_move = board.replace(0, point)
+                return board_after_move
+            elif point == 1:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+                if point == 0:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 1:
+                random_row = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_row  # Return the random number
+                random_col = random.randint(0, 4)  # Generate a random number between 0 and 4
+                return random_col  # Return the random number
+                point = (random_row, random_col)  # Create a tuple of the random numbers
+                return point  # Return the tuple
+                import class_board  # Import the class_board module
+                board = class_board.Board(5, 5)  # Create an instance of the Board class
+                return board  # Return the instance
+            elif point == 2:
+                board_after_move = board.replace(0, point)
+                return board_after_move
+            elif point == 2:
+                board_after_move = board.replace(0, point)
+                return board_after_move
+        elif point == 2:
+            board_after_move = board.replace(0, point)
+            return board_after_move
+
+
+
+    def evaluate_board(self, board):
+        # Implement the evaluation function to assess the current state of the board
         pass
 
     def evaluate_position(self, board):
