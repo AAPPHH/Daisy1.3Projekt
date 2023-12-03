78087f443e16a10aae2e86e4460bfa8d8eedd415
Marc-Maxelon <151784489+Marc-Maxelon@users.noreply.github.com>
2023-12-03 15:16:48 +0100

    Update class_bot_1.py
    Überarbeitung

--- class_bot_1.py
+++ class_bot_1.py
@@ -62,3 +62,23 @@ class GomokuBot(Player):
             if board[random_row][random_col] == 0:
                 board[random_row][random_col] = 2
                 return True
+
+
+class GomokuBot(Player):
+    def __init__(self, name, row, col):
+        self.name = name
+        self.row = row
+        self.col = col
+
+    def make_move(self, board):
+        import random
+        random_row = random.randint(0, 4)
+        random_col = random.randint(0, 4)
+        while board[random_row][random_col] != 0:
+            random_row = random.randint(0, 4)
+            random_col = random.randint(0, 4)
+        board[random_row][random_col] = 2
+
+
+
+
