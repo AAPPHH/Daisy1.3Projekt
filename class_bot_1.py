7f2634c519813c60e864d3a242d8d5644b635606
Marc-Maxelon <151784489+Marc-Maxelon@users.noreply.github.com>
2023-12-03 15:38:02 +0100

    Update class_bot_1.py
    Überarbeitung

--- class_bot_1.py
+++ class_bot_1.py
@@ -1,3 +1,6 @@
+import random
+import board
+
 class GomokuBot(Player):
     def __init__(self, name, row, col):
         self.name = name
@@ -5,7 +8,6 @@ class GomokuBot(Player):
         self.col = col
 
     def make_move(self, board):
-        import random
         random_row = random.randint(0, 4)
         random_col = random.randint(0, 4)
         while board[random_row][random_col] != 0:
