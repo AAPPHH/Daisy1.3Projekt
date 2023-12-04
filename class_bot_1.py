709d166f8d25ab5ac87eb1dbb9fce0e4b6f1e9d6
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-04 09:00:12 +0100

    Fix random move bug in GomokuBot class

--- class_bot_1.py
+++ class_bot_1.py
@@ -10,13 +10,3 @@ class GomokuBot(Player):
         random_row = random.randint(0, 4)
         random_col = random.randint(0, 4)
         self.place_piece(random_row, random_col)
-        
-    def evaluate_board(self, board):
-        # Implement the evaluation function to assess the current state of the board
-        pass
-
-    def evaluate_position(self, board):
-        # Implement the evaluation function to assess the current position on the board
-        pass
-
-
