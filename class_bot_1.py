4bceb0e7a8f8a7a6bd72a2583fcc5b48551a75d7
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-04 08:56:19 +0100

    Add random move functionality to GomokuBot

--- class_bot_1.py
+++ class_bot_1.py
@@ -1,11 +1,16 @@
+from class_player import *
+from class_board import * 
+import random
+class GomokuBot(Player):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-class GomokuBot:
-    def __init__(self, name, row, col):
-        self.name = name
-        self.row = row
-        self.col = col
 
-
+    def make_move(self):
+        random_row = random.randint(0, 4)
+        random_col = random.randint(0, 4)
+        self.place_piece(random_row, random_col)
+        
     def evaluate_board(self, board):
         # Implement the evaluation function to assess the current state of the board
         pass
