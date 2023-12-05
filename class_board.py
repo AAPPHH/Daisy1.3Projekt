b71598fdcddb9fef5737d09910f0caf2daa87e15
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-05 14:40:14 +0100

    Add winning condition for rows, columns, and diagonals

--- class_board.py
+++ class_board.py
@@ -16,26 +16,50 @@ class Board:
         if 0 <= row < m and 0 <= col < n and self.board[row][col] == 0:
             return True
         return False
-
-    def is_winner(self, piece): 
-        # Zeilen überprüfen
-        if np.any(np.all(self.board == piece, axis=1)):
-            return True
-
-        # Spalten überprüfen
-        if np.any(np.all(self.board == piece, axis=0)):
-            return True
-
-        # Hauptdiagonale überprüfen, wenn das Brett quadratisch ist
-        if self.board.shape[0] == self.board.shape[1]:
-            if np.all(np.diag(self.board) == piece):
+    
+    def is_winner(self, piece):
+        # Create a vector of length k with all elements being the piece
+        vec = np.ones(self.k, dtype=int) * piece
+        # Check rows
+        for row in self.board:
+            if np.any(np.convolve(row, vec, 'valid') == self.k):
                 return True
 
-            # Nebendiagonale überprüfen
-            if np.all(np.diag(np.fliplr(self.board)) == piece):
+        # Check columns
+        for col in self.board.T:
+            if np.any(np.convolve(col, vec, 'valid') == self.k):
+                return True
+
+        # Check main diagonal
+        for diag in [np.diagonal(self.board, offset) for offset in range(-self.board.shape[0]+self.k, self.board.shape[1]-self.k+1)]:
+            if np.any(np.convolve(diag, vec, 'valid') == self.k):
+                return True
+
+        # Check anti-diagonal
+        for diag in [np.diagonal(np.fliplr(self.board), offset) for offset in range(-self.board.shape[0]+self.k, self.board.shape[1]-self.k+1)]:
+            if np.any(np.convolve(diag, vec, 'valid') == self.k):
                 return True
 
         return False
+    # def is_winner(self, piece): 
+    #     # Zeilen überprüfen
+    #     if np.any(np.all(self.board == piece, axis=1)):
+    #         return True
+
+    #     # Spalten überprüfen
+    #     if np.any(np.all(self.board == piece, axis=0)):
+    #         return True
+
+    #     # Hauptdiagonale überprüfen, wenn das Brett quadratisch ist
+    #     if self.board.shape[0] == self.board.shape[1]:
+    #         if np.all(np.diag(self.board) == piece):
+    #             return True
+
+    #         # Nebendiagonale überprüfen
+    #         if np.all(np.diag(np.fliplr(self.board)) == piece):
+    #             return True
+
+    #     return False
     
     def is_full(self):
         return np.all(self.board != 0)
--- class_game.py
+++ class_game.py
@@ -14,7 +14,7 @@ class Game:
         self.current_player = self.player1
 
     def start(self):
-        print("Let's play five in row!/n Bitte geben sie ihre Namen ein:")
+        print("Let's play five in row!/n Bitte geben sie ihre Namen ein")
         time.sleep(1)
         self.player1.name = input("Spieler 1: ")
         time.sleep(1)
