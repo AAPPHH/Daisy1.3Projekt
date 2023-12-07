89a7c0d51b6fb696a344b60c28a8e1c9bb19cbd9
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-07 18:50:08 +0100

    Refactor GomokuBot class

--- class_bot_1.py
+++ class_bot_1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 from class_board import *
 from class_player import *
@@ -17,16 +16,3 @@ class GomokuBot(Player):
         #     random_row = random.randint(0, 4)
         #     random_col = random.randint(0, 4)
         # board[random_row][random_col] = 2
-=======
-from class_player import *
-from class_board import * 
-import random
-class GomokuBot(Player):
-    def __init__(self, name, player_number):
-        super().__init__(name, player_number)
-
-    def make_move(self):
-        random_row = random.randint(0, 4)
-        random_col = random.randint(0, 4)
-        self.place_piece(random_row, random_col)
->>>>>>> fe24b2b9de5070fd8619261f2943e284cbce99e1
