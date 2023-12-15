710b661b8c3d7889c3d7d91417ee70d0d3d652e1
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-15 18:00:39 +0100

    Add numpy import and update make_move method in MinimaxBot class

--- class_bot_3.py
+++ class_bot_3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from class_player import Player
 from class_board import Board
 
@@ -5,56 +7,59 @@ class MinimaxBot(Player):
     def __init__(self, name, player_number):
         super().__init__(name, player_number)
 
-    def make_move(self, board):
+    def make_move(self, row, col, game, board):
+        # Hinweis: row und col werden ignoriert, da der Bot seinen eigenen Zug berechnet
         best_score = float('-inf')
         best_move = None
         for move in self.get_possible_moves(board):
-            score = self.minimax(board, move, 3, False)
+            # Kopie des Bretts für die Simulation der Züge
+            board_copy = np.copy(board.board)
+            score = self.minimax(board_copy, move, 3, True)
             if score > best_score:
                 best_score = score
                 best_move = move
-        self.place_piece(best_move[0], best_move[1], board)
+
+        # Gültigkeit des besten Zuges prüfen und den Zug ausführen
+        if best_move and game.board.is_valid_move(best_move[0], best_move[1]):
+            game.board.place_piece(best_move[0], best_move[1], self.player_number)
+        else:
+            print("Kein gültiger Zug gefunden für MinimaxBot")
 
     def evaluate_board(self, board):
-        score = 0
-        for row in range(board.m):
-            for col in range(board.n):
-                if board.board[row][col] == self.player_number:
-                    score += 1
-                elif board.board[row][col] != 0:
-                    score -= 1
-        return score
+        # Einfache Bewertung: Zähle die Steine des Spielers
+        return np.sum(board == self.player_number) - np.sum((board != 0) & (board != self.player_number))
 
     def get_possible_moves(self, board):
-        possible_moves = []
-        for row in range(board.m):
-            for col in range(board.n):
-                if board.board[row][col] == 0:
-                    possible_moves.append((row, col))
-        return possible_moves
+        # Finde alle leeren Positionen für mögliche Züge
+        return [(row, col) for row, col in np.ndindex(board.shape) if board[row, col] == 0]
 
     def minimax(self, board, move, depth, is_maximizing):
-        board.board[move[0]][move[1]] = self.player_number if is_maximizing else 3 - self.player_number
+        board[move[0]][move[1]] = self.player_number if is_maximizing else 3 - self.player_number
 
-        if depth == 0 or board.is_game_over():
+        if depth == 0 or self.is_game_over(board):
             score = self.evaluate_board(board)
-            board.board[move[0]][move[1]] = 0
+            board[move[0]][move[1]] = 0  # Zug rückgängig machen
             return score
 
         if is_maximizing:
             max_eval = float('-inf')
             for next_move in self.get_possible_moves(board):
-                eval = self.minimax(board, next_move, depth - 1, False)
+                board_copy = np.copy(board)
+                eval = self.minimax(board_copy, next_move, depth - 1, False)
                 max_eval = max(max_eval, eval)
-            board.board[move[0]][move[1]] = 0
+            board[move[0]][move[1]] = 0  # Zug rückgängig machen
             return max_eval
-        
         else:
             min_eval = float('inf')
             for next_move in self.get_possible_moves(board):
-                eval = self.minimax(board, next_move, depth - 1, True)
+                board_copy = np.copy(board)
+                eval = self.minimax(board_copy, next_move, depth - 1, True)
                 min_eval = min(min_eval, eval)
-            board.board[move[0]][move[1]] = 0
+            board[move[0]][move[1]] = 0  # Zug rückgängig machen
             return min_eval
+
+    def is_game_over(self, board):
+        # Diese Methode sollte implementiert werden, um zu prüfen, ob das Spiel vorbei ist
+        pass  # Implementieren Sie die Logik basierend auf Ihrem Spiel
 
 
