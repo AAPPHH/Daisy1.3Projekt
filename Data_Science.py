ef00cfe54cf88f4117c5f9b4498f7f0cefa346bb
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2023-12-22 15:46:42 +0100

    Update user authentication logic
    The commit message suggests that the developer has made changes to the user authentication logic in the code.

--- Data_Science.py
+++ Data_Science.py
@@ -17,15 +17,16 @@ class Data_Science:
             # Returns an empty DataFrame if the file does not exist
             return pd.DataFrame()
 
-    def save_game_state(self, game):
-        # Convert game states to DataFrame
-        game_history = self.load_game_history()
-        for state in game.game_arrays:
-            game_history = game_history.append({
-                'player1': game.player1.name,
-                'player2': game.player2.name,
-                'board_state': state
-            }, ignore_index=True)
-        
-        # Save updated game history
-        self.save_game_history(game_history)
+def save_game_state(self, game):
+    # Convert game states to DataFrame
+    game_history = self.load_game_history()
+    for state in game.game_arrays:
+        new_row = pd.DataFrame([{
+            'player1': game.player1.name,
+            'player2': game.player2.name,
+            'board_state': state
+        }])
+        game_history = game_history.append(new_row, ignore_index=True)
+    
+    # Save updated game history
+    self.save_game_history(game_history)
