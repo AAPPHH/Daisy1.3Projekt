c55d93432298d27602eb3e96495da2e60deac7f4
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2023-12-28 22:28:34 +0100

    Update Data_Science.py
    Doc Strings 1

--- Data_Science.py
+++ Data_Science.py
@@ -1,20 +1,27 @@
-import pickle  # Import der Bibliothek "pickle"
-               # -> Speicherung von Data-Frames
-               # -> Serialisierung von Python-Instanzen
-               # -> Konvertierung von Daten in eine binäre Form
-               # -> ggf. Machine-Learning-Applikationen
+import pandas as pd
+import pickle
 
 class Data_Science:  # Erstellung der Klasse Data_Science
     def __init__(self, filename="game_history.pkl"):  # Konstruktor und Parameter: Name der Datei (Default)
         self.filename = filename  # -> self.filename = "game_history.pkl"
 
-    def save_game_history(self, game_history):  # Definition der Methode, die die gesammelten Daten speichert
-        with open(self.filename, "wb") as f:  # Öffnung der Datei im Binärmodus
-            pickle.dump(game_history, f)  # game_history wird in die Datei geladen
+    def save_game_state(self, game):  # Methode, die Spielzustände speichert
+        game_history = self.load_game_history()  # Ladung des bisherigen Spielverlaufes
+        for state in game.game_arrays:  # For-Schleife durchläuft jedes Element in game.game_arrays
+            new_row = pd.DataFrame([{  # Erstellung eines Data-Frames mit dem Namen "new_row", dictionary mit drei Keys
+                'player1': game.player1.name,
+                'player2': game.player2.name,
+                'board_state': state
+            }])
+            game_history = pd.concat([game_history, new_row], ignore_index=True)  # new_row wird dem DataFrame game_history hinzugefügt, concat: Verbindung der DataFrames, Index wird neu erstellt
+        self.save_game_history(game_history)  # Speicherung des aktualisierten Spielverlaufes
+    
 
-    def load_game_history(self):  # Definition der Methode, die 
-        try:  # Versuch...
-            with open(self.filename, "rb") as f:  # ... die im Binärmodus gespeicherten Daten zu öffnen...
-                return pickle.load(f)  # ... und zurückgegeben
-        except FileNotFoundError:  # Wird die Datei nicht gefunden, ...
-            return []  # ... dann wird eine leere Liste zurückgegeben
+    def save_game_history(self, game_history):
+        game_history.to_pickle(self.filename)
+
+    def load_game_history(self):
+        try:
+            return pd.read_pickle(self.filename)
+        except FileNotFoundError:
+            return pd.DataFrame()
