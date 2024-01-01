c27095c8322843520a848a07aa1230946efecc75
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2024-01-01 15:35:49 +0100

    Update class_bot_1.py
    Doc Strings

--- class_bot_1.py
+++ class_bot_1.py
@@ -4,10 +4,20 @@ from class_board import *
 import random
 
 class GomokuBot(Player):  # Erstellung der Klasse GomokuBot und Vererbung der Methode von Player
+    """
+    Erstellung der Klasse GomokuBot und Vererbung der Methode von Player.
+    Vererbung der Parameter.
+    """
     def __init__(self, name, player_number):
         super().__init__(name, player_number)  # Vererbung der Parameter
 
     def place_piece(self, game, board):  # Denition der Methode playe_piece mit drei Parametern
+        """
+        Denition der Methode playe_piece mit drei Parametern
+        row = zufällige Zahl zwischen 0 und 4
+        col = zufällige Zahl zwischen 0 und 4
+        Stein wird gemäß der Funktion place_piece gesetzt
+        """
         row = random.randint(0, 4)  # row = zufällige Zahl zwischen 0 und 4
         col = random.randint(0, 4)  # col = zufällige Zahl zwischen 0 und 4
         Player.place_piece(self, row, col, game, board)  # Stein wird gemäß der Funktion place_piece gesetzt
