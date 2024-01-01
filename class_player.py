b4727301ee2ed6dd25d4758fa574293df3d733e5
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2024-01-01 14:58:20 +0100

    Update class_player.py
    Grammar

--- class_player.py
+++ class_player.py
@@ -8,6 +8,13 @@ class Player:  # Erstellung der Klasse Player
 
 
     def place_piece(self, row, col, game, board):  # Definition der Methode, die den Stein setzt, Parameter: Zeilenzahl, Spaltenzahl, Spiel, Spielbrett
+        """
+        Definition der Methode, die den Stein setzt, Parameter: Zeilenzahl, Spaltenzahl, Spiel, Spielbrett.
+        Wenn die Eingabe (row, col) des Spielers gültig is (... == True), ...
+        ... dann entspricht das Feld auf dem Spielbrett der Spielernummer (vorher 0).
+        True wird zurückgegeben, sofern der Code entsprechend läuft.
+        Andernfalls (if nicht gültig) wird False (ungültiger Spielzug) zurückgegeben.
+        """
         if board.is_valid_move(row, col):  # Wenn die Eingabe (row, col) des Spielers gültig is (... == True), ...
             board.board[row][col] =  self.player_number  # ... dann entspricht das Feld auf dem Spielbrett der Spielernummer (vorher 0)
             return True  # True wird zurückgegeben, sofern der Code entsprechend läuft
