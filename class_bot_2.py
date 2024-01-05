2cbbecd7be9f80f5659bdc38cd2a146ca18d6d30
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2024-01-05 07:09:55 +0100

    Refactor TreeBot class constructor and make_move method

--- class_bot_2.py
+++ class_bot_2.py
@@ -3,15 +3,13 @@ import random
 from class_player import *
 
 class TreeBot(Player):
-    def __init__(self, m, n, board): 
-        self.m = m
-        self.n = n
-        self.board = np.zeros((m, n), dtype=int)
+    def __init__(self, name, player_number):
+        super().__init__(name, player_number)
 
-    # TreeBot.place_piece(self.current_player, row, col, self, self.board) 
-    def make_move(self, game, board):
-        if self.board[self.m // 2][self.n // 2] == 0:  # Zentrum
-            self.board[self.m // 2][self.n // 2] = 2
+    # TreeBot.place_piece(self.current_player, self, self.board) Parameter die reinkommen
+    def make_move(self, game, board): 
+        if self.board.board[self.m // 2][self.n // 2] == 0:  # Zentrum
+            self.board.board[self.m // 2][self.n // 2] = 2
         elif self.board[self.m // 2 + 1][self.n // 2] == 0:  # Oben von der Mitte
             self.board[self.m // 2 + 1][self.n // 2] = 2
         elif self.board[self.m // 2 - 1][self.n // 2] == 0:  # Unten von der Mitte
@@ -22,11 +20,9 @@ class TreeBot(Player):
             self.board[self.m // 2][self.n // 2 - 1] = 2
         else:
             exceptional_list = [self.m // 2, self.m // 2 + 1, self.m // 2 - 1, self.n // 2, self.n // 2 + 1, self.n // 2 - 1]
-            valid_rows = [i for i in range(self.m) if i not in exceptional_list]
-            valid_columns = [i for i in range(self.n) if i not in exceptional_list]
             random_row = random.choice(valid_rows)
             random_column = random.choice(valid_columns)
-            # self.board[random_row][random_column] = 2
-            Player.place_piece(self, random_row, random_column, game, board)
+            self.board[random_row][random_column] = 2
+            Player.place_piece(self, random_row, random_column, game, board) #Parameter die die Rausgehen
 
 
