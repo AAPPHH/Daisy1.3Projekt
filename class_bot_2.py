abcbd89bbbfda9058eca6b84f4200c82cf1665d5
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2024-01-05 18:21:57 +0100

    Update class_bot_2.py
    Update.

--- class_bot_2.py
+++ class_bot_2.py
@@ -38,3 +38,35 @@ class Silly(Player):
             random_row = random.choice(valid_rows)
             random_column = random.choice(valid_columns)
             self.board[random_row][random_column] = self.player_number
+
+
+----
+
+import numpy as np
+import random
+from class_player import *
+
+class TreeBot(Player):
+    def __init__(self, name, player_number):
+        super().__init__(name, player_number)
+
+    def make_move(self, game, board):
+            if board.board[board.m // 2][board.n // 2] == 0:
+                Player.place_piece(self, board.m // 2, board.n // 2, game, board)
+                print("Zentrum")
+            elif board.board[board.m // 2+1][board.n // 2+1] == 0:
+                Player.place_piece(self, board.m // 2+1, board.n // 2+1, game, board)
+                print("Oben von der Mitte")
+            elif board.board[board.m // 2-1][board.n // 2-1] == 0:
+                Player.place_piece(self, board.m // 2-1, board.n // 2-1, game, board)
+                print("Unten von der Mitte")
+            elif board.board[board.m // 2+2][board.n // 2+2] == 0:
+                Player.place_piece(self, board.m // 2+2, board.n // 2+2, game, board)
+                print("Rechts von der Mitte")
+            elif board.board[board.m // 2-2][board.n // 2-2] == 0:
+                Player.place_piece(self, board.m // 2-2, board.n // 2-2, game, board)
+                print("Links von der Mitte")
+            else:
+                row = random.randint(0, 4)
+                col = random.randint(0, 4)
+                Player.place_piece(self, row, col, game, board) #Parameter die die Rausgehen
