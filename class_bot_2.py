54e2424dae4f6cdd960d2c9f1c5723423f8e2ee3
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2024-01-05 13:56:49 +0100

    Remove commented out code and fix indentation in class_bot_2.py

--- class_bot_2.py
+++ class_bot_2.py
@@ -26,20 +26,3 @@ class TreeBot(Player):
                 row = random.randint(0, 4)
                 col = random.randint(0, 4)
                 Player.place_piece(self, row, col, game, board) #Parameter die die Rausgehen
-
-        # TreeBot.place_piece(self.current_player, self, self.board) Parameter die reinkommen
-        # if self.board.board[self.m // 2][self.n // 2] == 0:  # Zentrum
-        #     self.board.board[self.m // 2][self.n // 2] = 2
-        # elif self.board[self.m // 2 + 1][self.n // 2] == 0:  # Oben von der Mitte
-        #     self.board[self.m // 2 + 1][self.n // 2] = 2
-        # elif self.board[self.m // 2 - 1][self.n // 2] == 0:  # Unten von der Mitte
-        #     self.board[self.m // 2 - 1][self.n // 2] = 2
-        # elif self.board[self.m // 2][self.n // 2 + 1] == 0:  # Rechts von der Mitte
-        #     self.board[self.m // 2][self.n // 2 + 1] = 2
-        # elif self.board[self.m // 2][self.n // 2 - 1] == 0:  # Links von der Mitte
-        #     self.board[self.m // 2][self.n // 2 - 1] = 2
-        # else:
-            # exceptional_list = [self.m // 2, self.m // 2 + 1, self.m // 2 - 1, self.n // 2, self.n // 2 + 1, self.n // 2 - 1]
-            # random_row = random.choice(valid_rows)
-            # random_column = random.choice(valid_columns)
-            # self.board[random_row][random_column] = 2
