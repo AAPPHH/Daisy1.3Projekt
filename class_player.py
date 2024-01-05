0d673650089dccb117c0f10c3b5d7649a6bff03f
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2024-01-05 18:25:07 +0100

    Update class_player.py
    Update.

--- class_player.py
+++ class_player.py
@@ -19,3 +19,20 @@ class Player:  # Erstellung der Klasse Player
             board.board[row][col] =  self.player_number  # ... dann entspricht das Feld auf dem Spielbrett der Spielernummer (vorher 0)
             return True  # True wird zurückgegeben, sofern der Code entsprechend läuft
         return False  # Andernfalls (if nicht gültig) wird False (ungültiger Spielzug) zurückgegeben
+
+---
+
+
+from class_board import *
+
+class Player:
+    def __init__(self, name, player_number):
+        self.name = name
+        self.player_number = player_number
+
+
+    def place_piece(self, row, col, game, board):
+        if board.is_valid_move(row, col): 
+            board.board[row][col] =  self.player_number 
+            return True  
+        return False  
