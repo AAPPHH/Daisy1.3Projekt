d1d78b868fee4e351b6a29d525de32c0b92cba24
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2024-01-05 18:44:29 +0100

    Update class_player.py
    Update.

--- class_player.py
+++ class_player.py
@@ -1,28 +1,3 @@
-from class_player import *  # Allgemeiner Import der Klasse class_player
-from class_board import *  # Allgemeiner Import der Klasse class_board
-
-class Player:  # Erstellung der Klasse Player
-    def __init__(self, name, player_number):  # Konstruktor und Parameter: Name, Spielernummer
-        self.name = name
-        self.player_number = player_number
-
-
-    def place_piece(self, row, col, game, board):  # Definition der Methode, die den Stein setzt, Parameter: Zeilenzahl, Spaltenzahl, Spiel, Spielbrett
-        """
-        Definition der Methode, die den Stein setzt, Parameter: Zeilenzahl, Spaltenzahl, Spiel, Spielbrett.
-        Wenn die Eingabe (row, col) des Spielers gültig is (... == True), ...
-        ... dann entspricht das Feld auf dem Spielbrett der Spielernummer (vorher 0).
-        True wird zurückgegeben, sofern der Code entsprechend läuft.
-        Andernfalls (if nicht gültig) wird False (ungültiger Spielzug) zurückgegeben.
-        """
-        if board.is_valid_move(row, col):  # Wenn die Eingabe (row, col) des Spielers gültig is (... == True), ...
-            board.board[row][col] =  self.player_number  # ... dann entspricht das Feld auf dem Spielbrett der Spielernummer (vorher 0)
-            return True  # True wird zurückgegeben, sofern der Code entsprechend läuft
-        return False  # Andernfalls (if nicht gültig) wird False (ungültiger Spielzug) zurückgegeben
-
----
-
-
 from class_board import *
 
 class Player:
@@ -32,6 +7,13 @@ class Player:
 
 
     def place_piece(self, row, col, game, board):
+        """
+        Definition der Methode, die den Stein setzt, Parameter: Zeilenzahl, Spaltenzahl, Spiel, Spielbrett.
+        Wenn die Eingabe (row, col) des Spielers gültig is (... == True), ...
+        ... dann entspricht das Feld auf dem Spielbrett der Spielernummer (vorher 0).
+        True wird zurückgegeben, sofern der Code entsprechend läuft.
+        Andernfalls (if nicht gültig) wird False (ungültiger Spielzug) zurückgegeben.
+        """
         if board.is_valid_move(row, col): 
             board.board[row][col] =  self.player_number 
             return True  
