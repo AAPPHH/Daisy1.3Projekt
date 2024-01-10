05b48e73ab555fdb73b4db1a66f12bc45168d38a
Marc <151784489+Marc-Maxelon@users.noreply.github.com>
2024-01-10 11:45:52 +0100

    Update class_bot_5.py
    Update.

--- class_bot_5.py
+++ class_bot_5.py
@@ -8,7 +8,7 @@ class Blocker(Player):
         super().__init__(name, player_number)
 
     def check_enemy(self, board):
-        positions = np.where(self.board == 1)
+        positions = np.where(board == 1)
         enemy_positions = list(zip(positions[0], positions[1]))
         return enemy_positions
     
@@ -21,30 +21,30 @@ class Blocker(Player):
 
     def block_enemy(self, game, board, found_enemy, random_enemy_position):
         if found_enemy:
-            if self.board[random_enemy_position[0] - 1][random_enemy_position[1]] == 0:
-                Player.place_piece(self, random_enemy_position[0] - 1, random_enemy_position[1], game, board)
-            elif self.board[random_enemy_position[0] + 1][random_enemy_position[1]] == 0:
-                Player.place_piece(self, random_enemy_position[0] + 1, random_enemy_position[1], game, board)
-            elif self.board[random_enemy_position[0]][random_enemy_position[1] - 1] == 0:
-                Player.place_piece(self, random_enemy_position[0], random_enemy_position[1] - 1, game, board)
-            elif self.board[random_enemy_position[0]][random_enemy_position[1] + 1] == 0:
-                Player.place_piece(self, random_enemy_position[0], random_enemy_position[1] + 1, game, board)
-            elif self.board[random_enemy_position[0] - 1][random_enemy_position[1] - 1] == 0:
-                Player.place_piece(self, random_enemy_position[0] - 1, random_enemy_position[1] - 1, game, board)
-            elif self.board[random_enemy_position[0] - 1][random_enemy_position[1] + 1] == 0:
-                Player.place_piece(self, random_enemy_position[0] - 1, random_enemy_position[1] + 1, game, board)
-            elif self.board[random_enemy_position[0] + 1][random_enemy_position[1] - 1] == 0:
-                Player.place_piece(self, random_enemy_position[0] + 1, random_enemy_position[1] - 1, game, board)
-            elif self.board[random_enemy_position[0] + 1][random_enemy_position[1] + 1] == 0:
-                Player.place_piece(self, random_enemy_position[0] + 1, random_enemy_position[1] + 1, game, board)
+            if board.board[random_enemy_position[0] - 1][random_enemy_position[1]] == 0:
+                return Player.place_piece(self, random_enemy_position[0] - 1, random_enemy_position[1], game, board)
+            elif board.board[random_enemy_position[0] + 1][random_enemy_position[1]] == 0:
+                return Player.place_piece(self, random_enemy_position[0] + 1, random_enemy_position[1], game, board)
+            elif board.board[random_enemy_position[0]][random_enemy_position[1] - 1] == 0:
+                return Player.place_piece(self, random_enemy_position[0], random_enemy_position[1] - 1, game, board)
+            elif board.board[random_enemy_position[0]][random_enemy_position[1] + 1] == 0:
+                return Player.place_piece(self, random_enemy_position[0], random_enemy_position[1] + 1, game, board)
+            elif board.board[random_enemy_position[0] - 1][random_enemy_position[1] - 1] == 0:
+                return Player.place_piece(self, random_enemy_position[0] - 1, random_enemy_position[1] - 1, game, board)
+            elif board.board[random_enemy_position[0] - 1][random_enemy_position[1] + 1] == 0:
+                return Player.place_piece(self, random_enemy_position[0] - 1, random_enemy_position[1] + 1, game, board)
+            elif board.board[random_enemy_position[0] + 1][random_enemy_position[1] - 1] == 0:
+                return Player.place_piece(self, random_enemy_position[0] + 1, random_enemy_position[1] - 1, game, board)
+            elif board.board[random_enemy_position[0] + 1][random_enemy_position[1] + 1] == 0:
+                return Player.place_piece(self, random_enemy_position[0] + 1, random_enemy_position[1] + 1, game, board)
             else:
                 exceptional_list = [random_enemy_position[0] - 1, random_enemy_position[0] + 1, random_enemy_position[1] - 1, random_enemy_position[1] + 1]
                 valid_rows = [i for i in range(self.m) if i not in exceptional_list]
                 valid_columns = [i for i in range(self.n) if i not in exceptional_list]
                 random_row = random.choice(valid_rows)
                 random_column = random.choice(valid_columns)
-                Player.place_piece(self, random_row, random_column, game, board)
+                return Player.place_piece(self, random_row, random_column, game, board)
         else:
             rdm_row = random.randint(0, self.m - 1)
             rdm_column = random.randint(0, self.n - 1)
-            Player.place_piece(self, rdm_row, rdm_column, game, board)
+            return Player.place_piece(self, rdm_row, rdm_column, game, board)
