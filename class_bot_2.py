c1ed970b4b446026ed0470380e939c6041e882df
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2024-01-17 15:49:08 +0100

    Update Science.py and class_bot_2.py

--- class_bot_2.py
+++ class_bot_2.py
@@ -10,22 +10,21 @@ class TreeBot(Player):
         try:
             if board.board[board.m // 2][board.n // 2] == 0:
                 return Player.place_piece(self, board.m // 2, board.n // 2, game, board)
-            elif board.board[board.m // 2+1][board.n // 2+1] == 0:
-                return Player.place_piece(self, board.m // 2+1, board.n // 2+1, game, board)
-            elif board.board[board.m // 2-1][board.n // 2-1] == 0:
-                return Player.place_piece(self, board.m // 2-1, board.n // 2-1, game, board)         
-            elif board.board[board.m // 2+2][board.n // 2+2] == 0:
-                return Player.place_piece(self, board.m // 2+2, board.n // 2+2, game, board)
-            elif board.board[board.m // 2-2][board.n // 2-2] == 0:
-                return Player.place_piece(self, board.m // 2-2, board.n // 2-2, game, board)   
+            elif board.board[board.m // 2 + 1][board.n // 2 + 1] == 0:
+                return Player.place_piece(self, board.m // 2 + 1, board.n // 2 + 1, game, board)
+            elif board.board[board.m // 2 - 1][board.n // 2 - 1] == 0:
+                return Player.place_piece(self, board.m // 2 - 1, board.n // 2 - 1, game, board)
+            elif board.board[board.m // 2 + 2][board.n // 2 + 2] == 0:
+                return Player.place_piece(self, board.m // 2 + 2, board.n // 2 + 2, game, board)
+            elif board.board[board.m // 2 - 2][board.n // 2 - 2] == 0:
+                return Player.place_piece(self, board.m // 2 - 2, board.n // 2 - 2, game, board)
             else:
                 move = self.direction(board.board, self.player_number)
                 return Player.place_piece(self, move[0], move[1], game, board)
-        except:
-            row = random.randint(0, 4)
-            col = random.randint(0, 4)
-            return Player.place_piece(self, row, col, game, board)
-              
+        except Exception as e:
+            move = self.random_valid_move(board)
+            return Player.place_piece(self, move[0], move[1], game, board)
+
     def get_pos(self, board, player_number):
         chain = []
         for row_index, row in enumerate(board):
@@ -33,7 +32,7 @@ class TreeBot(Player):
                 if value == player_number:
                     chain.append((row_index, col_index))
         return chain
-    
+
     def direction(self, board, player_number):
         chain = self.get_pos(board, player_number)
         moves_list = []
@@ -53,22 +52,93 @@ class TreeBot(Player):
             for move in potential_moves:
                 if (0 <= move[0] < len(board) and 0 <= move[1] < len(board[0]) and
                         board[move[0]][move[1]] == 0 and
-                        (move[0], move[1] - 1) in chain or
-                        (move[0], move[1] + 1) in chain or
-                        (move[0] - 1, move[1]) in chain or
-                        (move[0] + 1, move[1]) in chain or
-                        (move[0] - 1, move[1] - 1) in chain or
-                        (move[0] + 1, move[1] + 1) in chain or
-                        (move[0] - 1, move[1] + 1) in chain or
-                        (move[0] + 1, move[1] - 1) in chain):
+                        ((move[0], move[1] - 1) in chain or
+                         (move[0], move[1] + 1) in chain or
+                         (move[0] - 1, move[1]) in chain or
+                         (move[0] + 1, move[1]) in chain or
+                         (move[0] - 1, move[1] - 1) in chain or
+                         (move[0] + 1, move[1] + 1) in chain or
+                         (move[0] - 1, move[1] + 1) in chain or
+                         (move[0] + 1, move[1] - 1) in chain)):
                     moves_list.append(move)
 
         if moves_list:
-            return random.choice(moves_list)   
-        else:     
-            row = random.randint(0, 4)
-            col = random.randint(0, 4)
-            return (row, col)
+            return random.choice(moves_list)
+        else:
+            return self.random_valid_move(board)
+
+    def random_valid_move(self, board):
+        valid_move_found = False
+        while not valid_move_found:
+            row = random.randint(0, board.m - 1)
+            col = random.randint(0, board.n - 1)
+            if board.board[row][col] == 0:
+                valid_move_found = True
+        return (row, col)
+
+    # def make_move(self, game, board):
+    #     try:
+    #         if board.board[board.m // 2][board.n // 2] == 0:
+    #             return Player.place_piece(self, board.m // 2, board.n // 2, game, board)
+    #         elif board.board[board.m // 2+1][board.n // 2+1] == 0:
+    #             return Player.place_piece(self, board.m // 2+1, board.n // 2+1, game, board)
+    #         elif board.board[board.m // 2-1][board.n // 2-1] == 0:
+    #             return Player.place_piece(self, board.m // 2-1, board.n // 2-1, game, board)         
+    #         elif board.board[board.m // 2+2][board.n // 2+2] == 0:
+    #             return Player.place_piece(self, board.m // 2+2, board.n // 2+2, game, board)
+    #         elif board.board[board.m // 2-2][board.n // 2-2] == 0:
+    #             return Player.place_piece(self, board.m // 2-2, board.n // 2-2, game, board)   
+    #         else:
+    #             move = self.direction(board.board, self.player_number)
+    #             return Player.place_piece(self, move[0], move[1], game, board)
+    #     except:
+    #         row = random.randint(0, 4)
+    #         col = random.randint(0, 4)
+    #         return Player.place_piece(self, row, col, game, board)
+              
+    # def get_pos(self, board, player_number):
+    #     chain = []
+    #     for row_index, row in enumerate(board):
+    #         for col_index, value in enumerate(row):
+    #             if value == player_number:
+    #                 chain.append((row_index, col_index))
+    #     return chain
+    
+    # def direction(self, board, player_number):
+    #     chain = self.get_pos(board, player_number)
+    #     moves_list = []
+
+    #     for row_index, col_index in chain:
+    #         potential_moves = [
+    #             (row_index, col_index + 2),
+    #             (row_index, col_index - 2),
+    #             (row_index + 2, col_index),
+    #             (row_index - 2, col_index),
+    #             (row_index + 2, col_index + 2),
+    #             (row_index - 2, col_index - 2),
+    #             (row_index + 2, col_index - 2),
+    #             (row_index - 2, col_index + 2)
+    #         ]
+
+    #         for move in potential_moves:
+    #             if (0 <= move[0] < len(board) and 0 <= move[1] < len(board[0]) and
+    #                     board[move[0]][move[1]] == 0 and
+    #                     (move[0], move[1] - 1) in chain or
+    #                     (move[0], move[1] + 1) in chain or
+    #                     (move[0] - 1, move[1]) in chain or
+    #                     (move[0] + 1, move[1]) in chain or
+    #                     (move[0] - 1, move[1] - 1) in chain or
+    #                     (move[0] + 1, move[1] + 1) in chain or
+    #                     (move[0] - 1, move[1] + 1) in chain or
+    #                     (move[0] + 1, move[1] - 1) in chain):
+    #                 moves_list.append(move)
+
+    #     if moves_list:
+    #         return random.choice(moves_list)   
+    #     else:     
+    #         row = random.randint(0, board.m)
+    #         col = random.randint(0, board.n)
+    #         return (row, col)
   # def direction(self, board, player_number):
     #     chain = self.get_pos(board, player_number)
     #     moves_list = []
