56e6913ae192a8392a955ee6fc0ced049e84686b
AAPPHH <147189916+AAPPHH@users.noreply.github.com>
2024-02-11 12:10:27 +0100

    Update comment to use "string" instead of "chain"

--- class_board.py
+++ class_board.py
@@ -23,7 +23,7 @@ class Board:
     
     def is_winner(self, piece):
         """
-        Checks if a player has won. i.e. if there is a chain of k pieces of the same player.
+        Checks if a player has won. i.e. if there is a string of k pieces of the same player.
         """
         win_sequence = str(piece) * self.k
         for row in self.array:
